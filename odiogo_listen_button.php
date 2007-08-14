bfcf23cc8e1cf7c29d45131904d7f114e54bef88
patricek <patricek@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2007-08-14 17:33:42 +0000

    Adding first version
    git-svn-id: https://plugins.svn.wordpress.org/listenbutton/trunk@16934 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ odiogo_listen_button.php
@@ -0,0 +1,252 @@
+<?php
+/*
+Plugin Name: Odiogo Listen Button
+Plugin URI: http://www.odiogo.com/download/wordpress/plugin/odiogo_listen_button_latest.php
+Description: Give your blog a voice! Add an "Odiogo Listen Button" so your readers can listen to your posts. <a href="http://www.odiogo.com/sign_up.php">Free Sign up</a>.
+Author: Odiogo
+Version: 1.6
+Author URI: http://www.odiogo.com/
+*/
+
+function odiogo_get_plugin_version ()
+{
+	return "1.6";
+}
+
+function odiogo_is_wp_head_template_bug_fix_enabled ()
+{
+	return true;
+}
+
+function odiogo_get_wp_static_pages ()
+{
+	global $wpdb;
+
+	// Retrieve the version of the installed WP
+	// WP 2.1 defines the pages differently from 2.0
+	$wp_version = substr (get_bloginfo ('version'), 0, 3);
+	if ($wp_version >= "2.1")
+	{
+		$pages = $wpdb->get_results ("select id, post_title
+						from $wpdb->posts
+						where post_status = 'publish'
+						and post_type = 'page'
+						order by post_title asc", 'ARRAY_A');
+	}
+	else
+	{
+		$pages = $wpdb->get_results ("select id, post_title
+						from $wpdb->posts
+						where post_status = 'static'
+						order by post_title asc", 'ARRAY_A');
+	}
+	return $pages;
+}
+
+function odiogo_is_wp_page_static ($id)
+{
+	$pages = odiogo_get_wp_static_pages ();
+	$result = false;
+
+	if ($pages)
+	{
+		foreach ($pages as $page)
+		{
+			if ($page['id'] == $id)
+			{
+				return true;
+			}
+		}
+	}
+	return $result;
+}
+
+function odiogo_get_option_odiogo_feed_id ()
+{
+	return get_option ('odiogo_feed_id');
+}
+
+function odiogo_get_option_odiogo_subscribe_button_title ()
+{
+	return get_option ('odiogo_subscribe_button_title');
+}
+
+function odiogo_plugin_options ()
+{
+	$odiogo_feed_id = trim (odiogo_get_option_odiogo_feed_id ());
+	$error_msg = strlen ($odiogo_feed_id) == 0 ? "You need to specify your Odiogo Feed ID.<br>" : "";
+
+	$str =
+'
+<div class="wrap">
+<h2>Odiogo Listen Button</h2>
+<h2>Options</h2>
+<form method="post">
+<p>
+<font color=red>' . $error_msg . '</font>
+<b>Your Odiogo Feed ID:</b> <input type="text" name="odiogo_feed_id" size="5" value="'. $odiogo_feed_id  . '">
+<a target=_blank href="http://www.odiogo.com/sign_up.php">Don\'t have an Odiogo Feed ID? Get It Here.</a>
+<br>
+(This is the number indicated in the activation email. Please <a target=_blank href="http://www.odiogo.com/company_contact_us.php">contact us</a> if you don\'t find it)
+<br>
+</p>
+<input type="hidden" name="odiogo_form1" value="odiogo_form1">
+<p><input type="submit" name="submit" value="Save  &raquo;"></p>
+</form>
+</div>
+';
+
+	echo $str;
+}
+
+function odiogo_listen_admin_menu ()
+{
+	add_options_page ('Odiogo Listen Button Options', 'Odiogo Listen Button', 8, __FILE__, 'odiogo_plugin_options');
+}
+
+function odiogo_listen_now_js ()
+{
+	$str =
+	'
+	<!-- BEGIN listen button odiogo.com v' . odiogo_get_plugin_version () . ' (WP) #1 -->
+	<script type="text/javascript" language="javascript" src="http://podcasts.odiogo.com/odiogo_js.php?feed_id=' . odiogo_get_option_odiogo_feed_id () . '&platform=wp"></script>
+	<!-- END listen button odiogo.com v' . odiogo_get_plugin_version () . ' (WP) #1 -->
+
+	';
+	return $str;
+}
+
+function odiogo_listen_now_1 ()
+{
+	$str = odiogo_listen_now_js ();
+	echo $str;
+}
+
+function odiogo_listen_now_2 ()
+{
+	global $wp_query;
+	$result = "";
+
+	if (! odiogo_is_wp_page_static ($wp_query->post->ID))
+	{
+		$str_post_title = $wp_query->post->post_title;
+
+		$str_post_title = str_replace ("\"", "", $str_post_title);
+		$str_post_title = str_replace ("'", "", $str_post_title);
+
+		$result = (odiogo_is_wp_head_template_bug_fix_enabled () ? odiogo_listen_now_js () : "") .
+		'
+		<!-- BEGIN listen button odiogo.com v' . odiogo_get_plugin_version () . ' (WP) #2 -->
+		<script type="text/javascript" language="javascript">
+		<!--
+		showOdiogoReadNowButton ("'. odiogo_get_option_odiogo_feed_id () .'", "' . $str_post_title . '", "' . $wp_query->post->ID . '", 290, 55);
+		//-->
+		</script>
+		<!-- END listen button odiogo.com v' . odiogo_get_plugin_version () . ' (WP) #2 -->
+
+		';
+	}
+	return $result;
+}
+
+function odiogo_listen_now_3 ()
+{
+	global $wp_query;
+	$result = "";
+
+	if (! odiogo_is_wp_page_static ($wp_query->post->ID))
+	{
+		$result =
+		'
+		<!-- BEGIN listen button odiogo.com v' . odiogo_get_plugin_version () . ' (WP) #3 -->
+		<script type="text/javascript" language="javascript">
+		<!--
+		showInitialOdiogoReadNowFrame ("'. odiogo_get_option_odiogo_feed_id () .'", "' . $wp_query->post->ID . '", 290, 0);
+		//-->
+		</script>
+		<!-- END listen button odiogo.com v' . odiogo_get_plugin_version () . ' (WP) #3 -->
+
+		';
+	}
+	return $result;
+}
+
+function odiogo_listen_content_filter ($content)
+{
+	if (is_feed ())
+	{
+		$result = $content;
+	}
+	else
+	{
+		$result = odiogo_listen_now_2 () . "<br>" . odiogo_listen_now_3 () . $content;
+	}
+	return $result;
+}
+
+function odiogo_process_post_form ()
+{
+	if ($_POST)
+	{
+		if ($_POST['odiogo_form1'])
+		{
+			update_option ('odiogo_feed_id', trim ($_POST['odiogo_feed_id']));
+		}
+	}
+}
+
+function odiogo_subscribe_button_control ()
+{
+	if ( $_POST['odiogo_subscribe_button-submit'] )
+	{
+		update_option ('odiogo_subscribe_button_title', trim ($_POST['odiogo_subscribe_button-title']));
+	}
+
+	echo '<p>';
+	echo 'Title: ';
+	echo '<input id="odiogo_subscribe_button-title" name="odiogo_subscribe_button-title" type="text" value="' . odiogo_get_option_odiogo_subscribe_button_title () . '">';
+	echo '<input type="hidden" id="odiogo_subscribe_button-form" name="odiogo_subscribe_button-submit" value="1">';
+	echo '</p>';
+
+}
+
+function odiogo_subscribe_button ($args)
+{
+	$str =
+'
+<ul>
+<li>
+<!-- BEGIN CODE ODIOGO SUBSCRIBE BUTTON -->
+' . odiogo_listen_now_js () . '
+<script type="text/javascript" language="javascript">
+<!--
+showOdiogoSubscribeButton (_odiogo_directory_name);
+//-->
+</script>
+<!-- END CODE ODIOGO SUBSCRIBE BUTTON -->
+</li>
+</ul>
+';
+	echo $args['before_widget'];
+	echo $args['before_title'] . odiogo_get_option_odiogo_subscribe_button_title () . $args['after_title'];
+	echo $str;
+	echo $args['after_widget'];
+}
+
+function odiogo_init_sidebar ()
+{
+	if (function_exists ('register_sidebar_widget') && function_exists ('register_widget_control'))
+	{
+		register_sidebar_widget ('Odiogo Subscribe Button', 'odiogo_subscribe_button');
+		register_widget_control ('Odiogo Subscribe Button', 'odiogo_subscribe_button_control');
+	}
+}
+
+add_action ('plugins_loaded', 'odiogo_init_sidebar');
+add_action ('admin_menu', 'odiogo_listen_admin_menu');
+add_action ('wp_head', 'odiogo_listen_now_1');
+add_filter ('the_content', 'odiogo_listen_content_filter');
+
+odiogo_process_post_form ();
+
+?>
---
+++ readme.txt
@@ -0,0 +1,73 @@
+=== Odiogo Listen Button ===
+Contributors: patricek
+Donate link: http://www.odiogo.com/
+Tags: podcast, mp3, text-to-speech, rss, feeds, audio, accessibility
+Requires at least: 2.0.5
+Tested up to: 2.2
+Stable tag: 2.2
+
+The 'Odiogo Listen Button' allows you to offer audio versions of your posts on-screen and to millions of iPod/MP3/mobile phone owners ... for free!
+
+== Description ==
+
+This plugin displays a 'listen' button underneath the title of each post. When the button is clicked, the post's 
+text is shifted a few lines down and a control bar is displayed from which the MP3 file can be played/paused. 
+The '+Odiogo' button underneath the player provides the user with many options such as subscribing to the
+podcast version of the blog, streaming the MP3 files, listening to older posts MP3 files, etc.
+
+There is also a sidebar 'Subscribe' button widget you can activate through the Sidbar Widgets Menu. Look for the 'Odiogo Subscribe Button' widget.
+
+Bloggers need to [register](http://www.odiogo.com/sign_up.php "Odiogo sign-up") (for free) their blog's RSS feed with Odiogo. Once in the Odiogo ecosystem, the feed 
+will be monitored on a regular basis and MP3 files will be automatically generated with the addition of new posts 
+to the blog. MP3 files are stored in the Odiogo environment. The Odiogo service is fully transparent and does not consume any bandwidth or disk space on the blog server system.
+
+The system is advertising based and is free for both bloggers and end users. When listenership reaches a significant level, a percentage of the revenue generated
+from advertising is redistributed to the blogger.
+
+
+== Installation ==
+
+1. Download the plugin `http://www.odiogo.com/download/wordpress/plugin/odiogo_listen_button_latest.php`
+2. Unzip the ZIP file into a temporary directory
+3. Copy the directory `odiogo_listen_button` into your `/wp-content/plugins/` directory via local file copy, FTP, scp or similar
+4. Login to your WordPress admin account and activate the ‘Odiogo Listen Button‘ plugin through the Plugins menu
+5. Click menu 'Options' > 'Odiogo Listen Button'
+6. Enter your 'Odiogo Feed ID' and click 'Save'
+7. Click menu 'Presention' > 'Widgets' (or 'Sidebar Widgets' depending on your WP version)
+8. Drag and drop 'Odiogo Subscribe Button' from 'Available Widgets' to 'Sidebar'
+
+
+== Frequently Asked Questions ==
+
+= What is Odiogo? =
+
+Odiogo enables you to convert your blog posts into high quality audio files. Check out the [demo](http://www.odiogo.com/demo.php).
+
+= What do I need to do to Odiogo-enable my content? =
+The procedure is extremely simple and should not take more than a few hours. You just need to fill out this 
+[form](http://www.odiogo.com/sign_up.php). An activation will be sent to you shortly after sign-up.
+Once your feed is enabled by Odiogo, you just need to promote the service on your home page and activate the Odiogo Listen Button plugin. 
+
+= How do I sign up? =
+
+Sign up is free and happens [here](http://www.odiogo.com/sign_up.php).
+
+= How do I get my Odiogo Feed ID? =
+
+The Feed ID will be communicated upon sign-up. 
+
+= What are the files produced by Odiogo for my feed? =
+Odiogo generates the following files:
+
+* A new RSS feed enriched with the Odiogo produced MP3 files. This is the feed your end-users would include in their podcast software such as iTunes or Juice.
+* An M3U file used to stream all the news item of your feed. This file can be opened with multimedia players such as Windows Media Player or Winamp.
+* "light" XHTML and WML pages. These can be accessed to download and listen to the mp3 files on a mobile phone over an Internet connection.
+* An HTML page that provides:
+   - Buttons to automatically subscribe to the podcast feed.
+   - Link to the M3U streaming file.
+   - List of all articles in the feed with a link to play each of the MP3 files.
+
+You can view an [example](http://podcasts.odiogo.com/prompt-speech-applications-weblog/podcasts-html.php) of this page.
+
+== Screenshots ==
+
